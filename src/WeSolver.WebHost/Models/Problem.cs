55f88f769c018895b4be17a0deec0160cd48ceba
unknown <Philly@.(none)>
2011-02-21 18:07:04 -0600

    Building up some models for Problem page

---
+++ src/WeSolver.WebHost/Models/Problem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeSolver.WebHost.Models
+{
+    public class Problem
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+
+        public Subset<Solution> Solutions { get; set; }
+
+        // public XmlQualifiedName Scale { get; set; }
+        // public XmlQualifiedName Priority { get; set; }
+        // public Subset<Tag> Tags { get; set; }
+        // public Subset<Discussion> Discussions { get; set; }
+        // public Subset<Reference> References { get; set; }
+    }
+}
---
+++ src/WeSolver.WebHost/Models/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeSolver.WebHost.Models
+{
+    public class Solution
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+    }
+}
