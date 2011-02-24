9abfa1d86b1a50f22fd2d2981040716e754a12fd
Jason <jason.rowe@gmail.com>
2011-02-23 23:30:59 -0600

    initial client side login work - requires host entry for testing locally

---
+++ src/WeSolver.WebHost/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeSolver.WebHost.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        //
+        // GET: /Registration/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            if (Request["signed_request"] != null)
+            {
+                var signedRequest = Request["signed_request"];
+            }
+
+            //TODO verify signed_request parameter from facebook
+            //TODO if valid setup user account in our DB using UID and email
+
+            return RedirectToAction("index", "home");
+        }
+    }
+}
