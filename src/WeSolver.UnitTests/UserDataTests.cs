ad5e48916bbc86697ef3b7740aad81ba33d49477
Jason <jason.rowe@gmail.com>
2011-03-10 21:23:01 -0600

    clean up UserDataTests

--- src/WeSolver.UnitTests/UserDataTests.cs
+++ src/WeSolver.UnitTests/UserDataTests.cs
@@ -7,23 +7,31 @@ namespace WeSolver.UnitTests
     [TestFixture, Category("integration")]
     public class UserDataTests
     {
-        [SetUp]
+        private WeSolverDataSource _dataSource;
+
+        [TestFixtureSetUp]
         public void Setup()
         {
-            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) =>
-            {
-                configSetter("UseDevelopmentStorage=true");
-            });
+            //local storage emulator needs to be running for these integration tests to work
+            CloudStorageAccount.SetConfigurationSettingPublisher((configName, configSetter) => configSetter("UseDevelopmentStorage=true"));
+            _dataSource = new WeSolverDataSource();
+        }
+
+        [TestFixtureTearDown]
+        public void CleanUp()
+        {
+            var ds = new WeSolverDataSource();
+            ds.DeleteUser("191921");
+            ds.DeleteUser("8178172");
         }
 
         [Test]
         public void SaveUser()
         {
             var testUser = new WeSolverUser { FacebookUid = "191921", Email = "jsonrow@somewhere.com" };
-            var ds = new WeSolverDataSource();
-            ds.AddUser(testUser);
+            _dataSource.AddUser(testUser);
 
-            Assert.That(ds.GetWeSolverUser("191921") != null, "save is not working");
+            Assert.That(_dataSource.GetWeSolverUser("191921") != null, "save is not working");
         }
 
         [Test]
@@ -31,14 +39,13 @@ namespace WeSolver.UnitTests
         {
             var testUser = new WeSolverUser { FacebookUid = "8178172", Email = "jsonrow@somewhere.com" };
 
-            var ds = new WeSolverDataSource();
-            ds.AddUser(testUser);
+            _dataSource.AddUser(testUser);
 
-            Assert.That(ds.GetWeSolverUser("8178172") != null, "save is not working");
+            Assert.That(_dataSource.GetWeSolverUser("8178172") != null, "save is not working");
 
-            ds.DeleteUser("8178172");
+            _dataSource.DeleteUser("8178172");
 
-            Assert.That(ds.GetWeSolverUser("8178172") == null, "delete is not working");
+            Assert.That(_dataSource.GetWeSolverUser("8178172") == null, "delete is not working");
         }
 
         [Test]
@@ -46,14 +53,13 @@ namespace WeSolver.UnitTests
         {
             var testUser = new WeSolverUser { FacebookUid = "8178172", Email = "jsonrow@somewhere.com" };
 
-            var ds = new WeSolverDataSource();
-            ds.AddUser(testUser);
+            _dataSource.AddUser(testUser);
 
             testUser.Bio = "@markrendle is cool";
 
-            ds.UpdateUser(testUser);
-
-            var result = ds.GetWeSolverUser("8178172");
+            _dataSource.UpdateUser(testUser);
+            
+            var result = _dataSource.GetWeSolverUser("8178172");
 
             Assert.That(result.Bio == "@markrendle is cool", "update didn't work");
         }
