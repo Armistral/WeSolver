9678efd46533376fd85e18bc8cc009fd86a66cee
unknown <Philly@.(none)>
2011-03-17 20:39:36 -0500

    Base navigation work

---
+++ src/WeSolver.WebHost/Models/NavItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace WeSolver.WebHost.Models
+{
+    public class NavItem
+    {
+        public string DisplayText { get; set; }
+        public string Action { get; set; }
+        public RouteValueDictionary Route { get; set; }
+        public bool Selected { get; set; }
+        public Func<RouteValueDictionary, bool> Condition { get; set; }
+
+        public IList<NavItem> Children { get; set; }
+    }
+}
